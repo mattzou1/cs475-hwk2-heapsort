9d708d8b76cd2c52fe1869d9667a54fea6566499
mattzou1 <matthewepiczou@gmail.com>
2023-01-27 22:55:45 +0000

    initial commit

--- heap.c
+++ heap.c
@@ -18,11 +18,18 @@
 void heapSort(Employee *A, int n)
 {
 	// TODO - BuildHeap on the heap
-
+	buildHeap(A, n);
 	// TODO - while n > 0:
-	// TODO - swap A[n-1] with A[0], since A[0] is the smallest number.
-	// TODO - A[n-1] now sorted in place, so decrement n
-	// TODO - Heapify the elements from A[0] up to A[n-1] (which leaves the newly sorted element alone)
+	while(n > 0){
+		// TODO - swap A[n-1] with A[0], since A[0] is the smallest number.
+		swap(&A[n-1], &A[0]); 
+		// TODO - A[n-1] now sorted in place, so decrement n
+		n--; 
+		// TODO - Heapify the elements from A[0] up to A[n-1] (which leaves the newly sorted element alone)
+		for(int i = 0; i < n; i++){
+			heapify(A, i, n);
+		}
+	}
 }
 
 /**
@@ -36,6 +43,9 @@ void heapSort(Employee *A, int n)
 void buildHeap(Employee *A, int n)
 {
 	// TODO - heapify() every element from A[n/2] down-to A[0]
+	for(int i = n/2; i >= 0; i--){
+		heapify(A, i, n);
+	}
 }
 
 /**
@@ -49,14 +59,35 @@ void buildHeap(Employee *A, int n)
 void heapify(Employee *A, int i, int n)
 {
 	// TODO - get index of left child of element i
+	int leftIndex = 2 * (i + 1) - 1; 
 	// TODO - get index of right child of element i
-
+	int rightIndex = 2 * (i + 1); 
 	// TODO - determine which child has a smaller salary. We'll call the index of this
 	//		element: "smaller"
-
+	int smallerIndex; 
+	if(leftIndex >= n && rightIndex >= n){
+		return; 
+	}
+	else if(rightIndex >= n){
+		smallerIndex = leftIndex;
+	}
+	else if(A[leftIndex].salary < A[rightIndex].salary){
+		smallerIndex = leftIndex;
+	}
+	else{
+		smallerIndex = rightIndex;
+	}
 	// TODO - recursively check if the salary at A[i] > the salary at A[smaller]. If it is, swap the two.
 	//			Then recursively heapify A[smaller].
+	if(A[i].salary > A[smallerIndex].salary){
+		Employee temp = A[i];
+		A[i] = A[smallerIndex]; 
+		A[smallerIndex] = temp; 
+	}
 	// TODO - Continue recursion as long as i is within range AND either right_child and left_child are still within range.
+	if(i < n && (leftIndex < n || rightIndex < n)){
+		heapify(A, smallerIndex, n); 
+	}
 }
 
 /**
@@ -67,6 +98,10 @@ void heapify(Employee *A, int i, int n)
 void swap(Employee *e1, Employee *e2)
 {
 	// TODO
+	Employee temp = *e1;
+	*e1 = *e2; 
+	*e2 = temp;  
+	
 }
 
 /**
@@ -76,5 +111,10 @@ void swap(Employee *e1, Employee *e2)
  */
 void printList(Employee *A, int n)
 {
+	printf("[id=%s sal=%d]", A[0].name, A[0].salary);
 	// TODO
+	for(int i = 1; i < n; i++){
+		printf(", [id=%s sal=%d]", A[i].name, A[i].salary);
+	}
+	printf("\n");
 }
--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include "employee.h"
 #include "heap.h"
+#define MAX_EMPLOYEES 5
 
 int main(int argc, char *argv[])
 {
-	//TODO
-	
+	Employee arr[MAX_EMPLOYEES]; 
+	for(int i = 0; i < MAX_EMPLOYEES; i++){ 
+		printf("Name: "); 
+		scanf("%s", arr[i].name); 
+		printf("Salary: "); 
+		scanf("%d", &arr[i].salary);
+		printf("\n");
+
+	}
+
+	printf("Before: ");
+	printList(arr, MAX_EMPLOYEES);
+	heapSort(arr, MAX_EMPLOYEES);
+	printf("After: ");
+	printList(arr, MAX_EMPLOYEES);
 	return 0;
 }
